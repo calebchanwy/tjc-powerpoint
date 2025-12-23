32236d797b12daa3bb7f6e50f32f90b7c4b4c5c0
Caleb Chan <calebchanwy@gmail.com>
2025-12-23 20:00:52 +0000

    Clean up SettingsForm.vb and fix max hymns setting save
    Addresses #15

--- src/TJCPowerPoint/SettingsForm.vb
+++ src/TJCPowerPoint/SettingsForm.vb
@@ -4,26 +4,24 @@
     Public Sub New()
         ' This call is required by the designer.
         InitializeComponent()
-        loadScreens()
-        loadMaxHymns()
+        LoadData()
     End Sub
 
-    Private Sub loadMaxHymns()
+    ''' <summary>
+    ''' Loads data and settings into form.
+    ''' </summary>
+    Private Sub LoadData()
         maxHCHymns.Value = My.Settings.maxHCHymns
         maxHymnalHymns.Value = My.Settings.maxHymnalHymns
         maxSermonHymns.Value = My.Settings.maxSermonHymns
-    End Sub
-    Private Sub loadScreens()
-        ' Get the list of screens
-        Dim screens As Screen() = Screen.AllScreens
 
-        ' Add each screen to the ComboBox
+        ' Load available screens to combobox
+        Dim screens As Screen() = Screen.AllScreens
         For Each screen As Screen In screens
-            ' Use the DeviceName property for numbering and Model property for the model name
             DisplayComboBox.Items.Add(screen.DeviceName & " - " & screen.Bounds.Width.ToString() & "x" & screen.Bounds.Height.ToString())
         Next
 
-        ' Load the previously selected display
+        ' Select default projector display
         Dim selectedDisplay As String = My.Settings.SelectedDisplay
 
         If Not String.IsNullOrEmpty(selectedDisplay) Then
@@ -42,6 +40,7 @@
             DisplayComboBox.SelectedIndex = 0
         End If
     End Sub
+
     Public Function getScreen() As Screen
         ' Get the selected screen
         Dim selectedScreen As Screen = Nothing
@@ -60,64 +59,39 @@
 
     End Function
 
-
-    Private Sub closeForm_Click(sender As Object, e As EventArgs)
-        Me.Hide()
-    End Sub
-    Private Sub minForm_Click(sender As Object, e As EventArgs)
-        Me.WindowState = FormWindowState.Minimized
-    End Sub
-
-    Private Sub DisplayComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayComboBox.SelectedIndexChanged
-        saveScreen()
-
-    End Sub
-
-    Private Sub maxHymnsChanged(sender As Object, e As EventArgs) Handles maxHCHymns.ValueChanged, maxHymnalHymns.ValueChanged, maxSermonHymns.ValueChanged
-        saveMaxHymns()
-    End Sub
-
-    Private Sub saveBtn_Click(sender As Object, e As EventArgs) Handles saveBtn.Click
-        saveSettings()
-    End Sub
-
-    'Method that will save all settings
-    Private Sub saveSettings()
-        saveScreen()
+    ''' <summary>
+    ''' Saves max hymns displayed to settings
+    ''' </summary>
+    Private Sub SaveSettings()
+        SaveScreen()
         saveMaxHymns()
         MessageBox.Show("Save Successful", "Save Successful")
     End Sub
-    'Method to save the settings of all maximum hymns
-    Private Sub saveMaxHymns()
-        'For each updown, check if text are all integers, if any are not integers return error
-        Dim updowns() = {maxSermonHymns, maxHymnalHymns, maxHCHymns}
-        For Each updown In updowns
-            If updown.Name.Equals("maxSermonHymns") Then
-                My.Settings.maxSermonHymns = updown.Value
-            ElseIf updown.Name.Equals("maxHymnalHymns") Then
-                My.Settings.maxHymnalHymns = updown.Value
-            ElseIf updown.Name.Equals("maxHCHymns") Then
-                My.Settings.maxHCHymns = updown.Value
-            End If
-        Next
+    ''' <summary>
+    ''' Saves max hymns displayed to settings
+    ''' </summary>
+    Private Sub SaveMaxHymns()
+        My.Settings.Item("maxSermonHymns") = CInt(maxSermonHymns.Value)
+        My.Settings.Item("maxHymnalHymns") = CInt(maxHymnalHymns.Value)
+        My.Settings.Item("maxHCHymns") = CInt(maxHCHymns.Value)
+        My.Settings.Save()
     End Sub
-    'Method to save the selected screen
-    Private Sub saveScreen()
-        ' Get the selected screen
-        Dim selectedScreen As Screen = Screen.AllScreens(DisplayComboBox.SelectedIndex)
 
-        ' Save the selected display
+    ''' <summary>
+    ''' Saves selected display to settings
+    ''' </summary>
+    Private Sub SaveScreen()
+        Dim selectedScreen As Screen = Screen.AllScreens(DisplayComboBox.SelectedIndex)
         My.Settings.SelectedDisplay = selectedScreen.DeviceName
         My.Settings.Save()
-
     End Sub
 
-    'Ensures that upon closing, the form state is still saved
-    Private Sub SettingsForm_Closing(sender As Object, e As FormClosingEventArgs) Handles MyBase.Closing
-        Hide()
-        e.Cancel = True
+    Private Sub DisplayComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayComboBox.SelectedIndexChanged
+        SaveScreen()
     End Sub
-
+    Private Sub saveBtn_Click(sender As Object, e As EventArgs) Handles saveBtn.Click
+        SaveSettings()
+    End Sub
     Private Sub openFolderBtn_Click(sender As Object, e As EventArgs) Handles openFolderBtn.Click
         Process.Start(MainProgram.getCurrentDirectory())
     End Sub
